82db68c418d82669f24226a591528eefbec46d7e
scriptingmaverick <khasshaik12@gmail.com>
2026-05-23 00:26:26 +0530

    Update the model as a document

--- src/main/java/com/hotel/Hotel/Hotel.java
+++ src/main/java/com/hotel/Hotel/Hotel.java
@@ -1,8 +1,10 @@
 package com.hotel.Hotel;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
+@Document(value = "hotel")
 public class Hotel {
   private int rooms;
   @Id
